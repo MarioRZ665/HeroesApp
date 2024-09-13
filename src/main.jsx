8f6d3b58656affbd93c5ff3b42e14872be77f143
Mario Rosas <mariorz665@outlook.com>
2024-09-12 21:24:31 -0600

    new files

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import './styles.css'
+import { HeroesApp } from './HeroesApp'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <HeroesApp></HeroesApp>
   </StrictMode>,
 )
