53d1a44f84900023ea0d18f7b33b2993786aa9f6
Mario Rosas <mariorz665@outlook.com>
2024-09-20 11:40:37 -0600

    navbar

--- src/HeroesApp.jsx
+++ src/HeroesApp.jsx
@@ -1,22 +1,9 @@
+import { AppRouter } from "./router/AppRouter";
+
 export const HeroesApp = () => {
 	return (
-		<div className="container">
-			<header className="bg-primary text-white text-center p-3">
-				<h1>Bem-vindo ao Meu App React</h1>
-			</header>
-			<main className="mt-4">
-				<div className="row">
-					<div className="col-md-6">
-						<div className="card">
-							<div className="card-body">
-								<h5 className="card-title">Título do Card</h5>
-								<p className="card-text">Um exemplo rápido de texto para construir o título do card e compor o conteúdo do card.</p>
-								<a href="#" className="btn btn-primary">Ir para algum lugar</a>
-							</div>
-						</div>
-					</div>
-				</div>
-			</main>
-		</div>
+		<>
+		<AppRouter />
+		</>
 	);
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './styles.css'
 import { HeroesApp } from './HeroesApp'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <BrowserRouter>
     <HeroesApp></HeroesApp>
+    </BrowserRouter>
   </StrictMode>,
 )
