89e448c4eb7640fd06bf00b825598409df1babb6
Mario Rosas <mariorz665@outlook.com>
2024-09-20 14:48:19 -0600

    Create to secon router

---
+++ src/heroes/index.js
@@ -0,0 +1,5 @@
+export *from './pages/DCPage.jsx';
+export *from './pages/MarvelPage.jsx'
+export *from './routers/HeroesRouters.jsx'
+export *from './pages/SearchPage.jsx'
+export *from './pages/HeroPage.jsx'
---
+++ src/heroes/routers/HeroesRouters.jsx
@@ -0,0 +1,19 @@
+import { Navigate, Route, Routes } from "react-router"
+import { Navbar } from "../../ui"
+import { MarvelPage, DCPage, SearchPage, HeroPage } from "../../heroes"
+export const HeroesRouters = () => {
+	return (
+		<>
+			<Navbar />
+			<div className="container">
+				<Routes>
+					<Route path="marvel" element={<MarvelPage />} />
+					<Route path="dc" element={<DCPage />} />
+					<Route path="search" element={<SearchPage />} />
+					<Route path="heroe" element={<HeroPage />} />
+					<Route path="/" element={<Navigate to="/marvel" />} />
+				</Routes>
+			</div>
+		</>
+	);
+}
