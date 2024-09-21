d1a0aa5d22736d71a36d71f3c63dd202894889b5
Mario Rosas <mariorz665@outlook.com>
2024-09-21 12:26:26 -0600

    add navbar

--- src/ui/components/Navbar/Navbar.tsx
+++ src/ui/components/Navbar/Navbar.tsx
@@ -1,26 +1,33 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 export const Navbar = () => {
+
+	const navigate = useNavigate();
+
+	const onLogout = () => {
+		navigate('/login',{
+			replace:true
+		});
+	}
+
 	return (
 		<nav className="navbar navbar-expand-sm navbar-dark bg-dark">
-
 			<Link
 				className="navbar-brand"
 				to="/"
 			>
 				Asociaciones
 			</Link>
-
 			<div className="navbar-collapse">
 				<div className="navbar-nav">
 					<NavLink
-						className={({isActive}) =>   `nav-item nav-link ${isActive ? 'active':''}`}
+						className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`}
 						to="/marvel"
 					>
 						Marvel
 					</NavLink>
 					<NavLink
-						className={({isActive}) =>   `nav-item nav-link ${isActive ? 'active':''}`}
+						className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`}
 						to="/dc"
 					>
 						DC
@@ -33,7 +40,9 @@ export const Navbar = () => {
 					<span className='nav-item nav-link text-info'>
 						Mario Rosas
 					</span>
-					<button className='nav-item nav-link btn'>
+					<button
+						className='nav-item nav-link btn'
+						onClick={onLogout}>
 						Logout
 					</button>
 				</ul>
