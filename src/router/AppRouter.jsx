000721e8651eedfa21ee042bc0b45f1f238db727
Mario RZ <49970661+MarioRZ665@users.noreply.github.com>
2024-10-01 23:31:43 -0600

    crear ruta privada

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,13 +1,19 @@
 import {Route, Routes } from "react-router";
 import {HeroesRouters } from "../heroes";
 import { LoginPage } from "../auth";
+import { PrivateRoute } from "./PrivateRoute";
 
 export const AppRouter = () => {
 	return (
 		<>
 			<Routes>
 				<Route path="login" element={<LoginPage />} />
-				<Route path="/*" element={<HeroesRouters />} />
+				<Route path="*" element={
+					<PrivateRoute>
+						<HeroesRouters />
+					</PrivateRoute>
+				} />
+				{/*<Route path="/*" element={<HeroesRouters />} />*/}
 			</Routes>
 		</>
 	);
